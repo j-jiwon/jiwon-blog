b850e84752f017382c5f53060048a48d63604274
JIWON <ziwon16@gmail.com>
2020-05-08 18:05:53 +0900

    Fix Image Error

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.createPages = async ({ graphql, actions }, themeOptions) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
